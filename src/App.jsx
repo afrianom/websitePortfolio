c152ffb8092633c5b6edcc6b90482581f6602bfd
piperiao <andresfeliperianom@gmail.com>
2024-07-13 23:24:31 -0500

    add the scroll module

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
+import React from 'react'
 import './App.css'
+import logoAfriano from './assets/logosolo.svg';
+import Scroll from './modules/scroll/Scroll';
 
 function App() {
   
 
   return (
-    <div>
-
+    <div className='app-main'>
+      <img src={logoAfriano} className='logo-app'></img>
+      <h2 className="subtitle">Portfolio of websites</h2>
+      <Scroll/>
     </div>
   )
 }
---
+++ src/modules/scroll/Scroll.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './scroll.css'
+
+const Scroll = ()=> {
+    return (
+        <div className="scroll">
+            <div className="scroll-item1">esto es una de las paginas</div>
+            <div className="scroll-item2"></div>
+            <div className="scroll-item3"></div>
+        </div>
+    )
+}
+
+export default Scroll;
