3b127bea3e1fa6bfcc29d84f98633a476fd0712f
piperiao <andresfeliperianom@gmail.com>
2024-07-14 18:22:56 -0500

    add the websites scroll

--- src/modules/scroll/Scroll.jsx
+++ src/modules/scroll/Scroll.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import './scroll.css'
+import websitesArray from "./websitesArray/websitesArray";
 
 const Scroll = ()=> {
     return (
         <div className="scroll">
-            
+            {websitesArray.map((page) => (
+                <div key={page.web_no} className="scroll-items">
+                    <img src={page.web_img} width='250' />
+                    <p>{page.web_name}</p>
+                </div>
+            ))}
         </div>
     )
 }
---
+++ src/modules/scroll/websitesArray/websitesArray.js
@@ -0,0 +1,34 @@
+import clubCampPic from './websitePictures/clubCampWeb.jpg';
+import adventureTourPic from './websitePictures/adventureWeb.jpg';
+import caciquePic from './websitePictures/caciqueWeb.jpg';
+import yogaStudioPic from './websitePictures/yogaWeb.jpg';
+import draAnaPic from './websitePictures/draAnaWeb.jpg';
+
+const websitesArray = [
+    {   web_no: 1,
+        web_name: 'Club campestre',
+        web_img: clubCampPic,
+    },
+    {
+        web_no: 2,
+        web_name: 'Adventure tours Website',
+        web_img: adventureTourPic
+    },
+    {
+        web_no: 3,
+        web_name: 'Cacique de la Junta',
+        web_img: caciquePic
+    },
+    {
+        web_no: 4,
+        web_name: 'Yoga Studio',
+        web_img: yogaStudioPic
+    },
+    {
+        web_no: 5,
+        web_name: 'Dra Ana Becerril website',
+        web_img: draAnaPic
+    }
+]
+
+export default websitesArray;
