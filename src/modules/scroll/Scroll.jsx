f6afa9a8ea48cbc8b5abb4ae6b1697871e43f740
piperiao <andresfeliperianom@gmail.com>
2024-07-13 23:35:45 -0500

    design and choose the right colors

--- src/modules/scroll/Scroll.jsx
+++ src/modules/scroll/Scroll.jsx
@@ -4,9 +4,7 @@ import './scroll.css'
 const Scroll = ()=> {
     return (
         <div className="scroll">
-            <div className="scroll-item1">esto es una de las paginas</div>
-            <div className="scroll-item2"></div>
-            <div className="scroll-item3"></div>
+            
         </div>
     )
 }
